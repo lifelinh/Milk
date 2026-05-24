8375da097d2a4c41e5e7f7bf7ba81c081be70147
miffy <miffy@bu.edu>
2026-05-24 00:19:14 -0400

    Change loadstring to return loadstring

--- modules/Auto-Incubate.lua
+++ modules/Auto-Incubate.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/the-amazing-digital-circus/Milk/main/modules/Auto-Incubate.lua"))()
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/the-amazing-digital-circus/Milk/main/modules/Auto-Incubate.lua"))()
