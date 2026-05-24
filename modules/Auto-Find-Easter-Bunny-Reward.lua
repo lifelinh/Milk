ccf6becfe128c5c5bf09fe1db578919dc2d51a0e
lifelinh <miffy@bu.edu>
2026-05-24 04:14:06 -0400

    Module 4: Auto-Find Easter Bunny Reward

---
+++ modules/Auto-Find-Easter-Bunny-Reward.lua
@@ -0,0 +1,175 @@
+local Starlight = loadstring(game:HttpGet("https://raw.githubusercontent.com/the-amazing-digital-circus/Starlight-UI-Milk/master/Source.lua"))()  
+local NebulaIcons = loadstring(game:HttpGet("https://raw.githubusercontent.com/the-amazing-digital-circus/Nebula-Icons-Milk/master/Loader.luau"))()
+
+if not game:IsLoaded() then 
+    game.Loaded:wait()
+end
+
+local InsertService = game:GetService("InsertService")
+local VirtualUser = game:GetService("VirtualUser")
+local TeleportService = game:GetService("TeleportService")
+local GuiService = game:GetService("GuiService")
+local RunService = game:GetService("RunService")
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local PlayerGui = LocalPlayer.PlayerGui
+
+Starlight:SetTheme("Hollywood Fluent")
+
+local Window = Starlight:CreateWindow({
+    Name = "Milk",
+    Subtitle = "discord.gg/9NyRdmfTgp",
+	Icon = "105193356396598",
+	LoadingEnabled = true,
+    LoadingSettings = {
+        Title = "Milk",
+        Subtitle = "Welcome!",
+		Logo = "82490480792476"
+    },
+    FileSettings = {
+        ConfigFolder = "C4Cfgs"
+    }
+})
+
+Window:CreateHomeTab({
+    SupportedExecutors = {"Opiumware", "MacSploit", "Delta", "Xeno", "Solara", "Seliware", "Madium"},
+    UnsupportedExecutors = {},
+    DiscordInvite = "9NyRdmfTgp",
+    Backdrop = "81840397943482",
+    IconStyle = 1,
+    Changelog = {
+        {
+            Title = "Module 4 'Auto Easter Bunny'",
+            Date = "24 May 2026",
+            Description = "Open source Auto-Find Easter Bunny egg!",
+        }
+    }
+})
+
+local TabSection = Window:CreateTabSection("Panels")
+
+local Auto = TabSection:CreateTab({
+    Name = "Automation",
+    Icon = NebulaIcons:GetIcon("refresh-cw", "Lucide"),
+    Columns = 1,
+}, "Automation Tab")
+
+local MiscGroupbox = Auto:CreateGroupbox({
+    Name = "Miscellaneous",
+    Icon = NebulaIcons:GetIcon("gem", "Lucide"),
+    Column = 1,
+}, "Misc Groupbox")
+
+local Settings = TabSection:CreateTab({
+    Name = "Settings",
+    Icon = NebulaIcons:GetIcon("wrench", "Lucide"),
+    Columns = 1,
+}, "Settings Tab")
+
+local GameSettings = Settings:CreateGroupbox({
+	Name = "Game",
+	Icon = NebulaIcons:GetIcon("computer", "Lucide"),
+	Column = 1,
+}, "Game Settings")
+
+local EasterBunnyLoopRunning
+local AutoEasterBunnyEnabled
+local EasterEggRewardConnection
+
+local function AutoEasterBunnyLoop()
+	if EasterBunnyLoopRunning then
+		return
+	end
+	EasterBunnyLoopRunning = true
+	local Character = LocalPlayer.Character
+	for _, Object in ipairs(workspace:GetChildren()) do
+		if not AutoEasterBunnyEnabled then
+			break
+		end
+		if Object.Name == "EasterEggReward" then
+			Character:PivotTo(Object:GetPivot() + Vector3.new(0, 5, 0))
+			task.wait(0.2)
+		end
+	end
+	EasterEggRewardConnection = workspace.ChildAdded:Connect(function(Child)
+		if not AutoEasterBunnyEnabled then
+			EasterBunnyLoopRunning = false
+			EasterEggRewardConnection:Disconnect()
+			EasterEggRewardConnection = nil
+			return
+		end
+		if Child.Name == "EasterEggReward" then
+			Character:PivotTo(Child:GetPivot() + Vector3.new(0, 5, 0))
+		end
+	end)
+end
+
+LocalPlayer.Idled:Connect(function()
+	task.wait(5)
+	VirtualUser:Button2Down(Vector2.zero, workspace.CurrentCamera.CFrame)
+	task.wait(1)
+	VirtualUser:Button2Up(Vector2.zero, workspace.CurrentCamera.CFrame)
+	task.wait(5)
+end)
+
+local function OnErrorMessageChanged(ErrorMessage)
+    if ErrorMessage then
+        task.wait(1)
+        TeleportService:Teleport(game.PlaceId, LocalPlayer)
+    end
+end
+GuiService.ErrorMessageChanged:Connect(OnErrorMessageChanged)
+
+local AutoEasterBunny = MiscGroupbox:CreateToggle({
+    Name = "Auto-Find Easter Bunny Reward",
+    CurrentValue = false,
+    Style = 2,
+    Callback = function(Value)
+        AutoEasterBunnyEnabled = Value
+        if Value then
+            task.spawn(AutoEasterBunnyLoop)
+        end
+    end,
+}, "Auto-Find Easter Bunny Toggle")
+
+local DeRender = GameSettings:CreateToggle({
+	Name = "Disable 3D Rendering",
+	CurrentValue = false,
+	Style = 2,
+	Callback = function(Value)
+		RunService:Set3dRenderingEnabled(not Value)
+	end,
+}, "Unrender World")
+
+local NoGui = GameSettings:CreateButton({
+	Name = "Hide GUIs (must rejoin to undo)",
+	Icon = NebulaIcons:GetIcon("eye-off", "Lucide"),
+	Callback = function()
+		for _, Element in pairs(PlayerGui:GetChildren()) do
+			if Element:IsA("ScreenGui") then
+				if Element.Name == "Sheckles_UI" or Element.Name == "ChocCoinCurrency_UI" then
+					Element:Destroy()
+				else
+					Element.Enabled = false
+				end
+			end
+		end
+	end,
+}, "Hide GUIs")
+
+local Rejoin = GameSettings:CreateButton({
+	Name = "Rejoin Game",
+	Icon = NebulaIcons:GetIcon("wifi-sync", "Lucide"),
+	Callback = function()
+		TeleportService:Teleport(game.PlaceId, LocalPlayer)
+	end,
+}, "Rejoin Game")
+
+Starlight:OnDestroy(function()
+    AutoEasterBunnyLoop = nil
+    AutoEasterBunnyEnabled = nil
+    RunService:Set3dRenderingEnabled(true)
+end)
+
+Settings:BuildConfigGroupbox()
+Starlight:LoadAutoloadConfig()
