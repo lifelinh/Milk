4d6938afc4c260f1f698cfd25bc0f01fdf547946
miffy <miffy@bu.edu>
2026-05-24 00:19:04 -0400

    Update Auto-Craft.lua

--- modules/Auto-Craft.lua
+++ modules/Auto-Craft.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/the-amazing-digital-circus/Milk/main/modules/Auto-Craft.lua"))()
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/the-amazing-digital-circus/Milk/main/modules/Auto-Craft.lua"))()
